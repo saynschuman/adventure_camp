27019f13dab3401d1052130e848b849cc14687bb
saynschuman <saynschuman@gmail.com>
2017-06-13 19:03:52 +0300

    f

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 (function($) {
     
-	
+	$(window).on('load', function(){
+		$('tr').addClass('tr');
+		$('tr td:first-child').addClass('first');
+		$('tr td:last-child').addClass('last');
+		
+		$('#sort').click(function(e){
+			e.preventDefault();
+			
+			first = [];
+			second = [];
+			third = [];
+			fourth = [];
+			fifth = [];
+			sixth = [];
+		})
+	})
 
 })(jQuery);
 
